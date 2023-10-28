b36c18771b4fec05a889f3672d734d0a1d1fc741
MSA <crystalbluetech@gmail.com>
2023-10-28 21:21:43 +0100

    fix all issues

--- db/migrate/20231025092854_create_groups.rb
+++ db/migrate/20231025092854_create_groups.rb
@@ -1,7 +1,7 @@
 class CreateGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :groups do |t|
-      t.references :user, null: false, foreign_key: { to_table: :users }
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.string :name
       t.string :icon
       
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_25_093104) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_28_195520) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,12 +33,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_093104) do
   end
 
   create_table "groups", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.string "name"
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_groups_on_user_id"
+    t.index ["author_id"], name: "index_groups_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,6 +49,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_093104) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -56,5 +62,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_25_093104) do
   add_foreign_key "expense_groups", "expenses"
   add_foreign_key "expense_groups", "groups"
   add_foreign_key "expenses", "users", column: "author_id"
-  add_foreign_key "groups", "users"
+  add_foreign_key "groups", "users", column: "author_id"
 end
